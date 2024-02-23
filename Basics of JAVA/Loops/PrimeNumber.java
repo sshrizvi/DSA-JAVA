bed44b92de73334ac5c834c9c02cf34d60d5fdbe
Syed Shujaat Haider <shujaathaider92@gmail.com>
2024-02-23 04:41:44 -0800

    Loops Started

---
+++ Basics of JAVA/Loops/PrimeNumber.java
@@ -0,0 +1,29 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num, flag = 0;
+
+        System.out.println("Enter a number : ");
+        num = sc.nextInt();
+
+        //Logic
+        for(int i = 2; i < num; i++){
+            if(num % i == 0){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1 || num == 1)
+            System.out.println("Not a prime number.");
+        else
+            System.out.println("It is a prime number.");
+
+        //Closing object of Scanner class
+        sc.close();
+    }
+}
---
+++ Basics of JAVA/Loops/ReverseNumber.java
@@ -0,0 +1,23 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num, rev = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        num = sc.nextInt();
+        
+        //Logic
+        while(num > 0){
+            rev = (rev * 10) + (num % 10);
+            num = num / 10;
+        }
+
+        System.out.println("Reverse of given number is " + rev);
+
+        //Closing object of Scanner class
+        sc.close();
+    }
+}
