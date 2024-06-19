628aefaeb97d6d38c06dc187794d4abcb1554620
Syed Shujaat Haider <shujaathaider92@gmail.com>
2024-06-19 21:28:01 +0530

    Updated Maximum Jumps

--- IntermediateJAVA/Arrays/Striver's Array Series/MinimumJumps.java
+++ IntermediateJAVA/Arrays/Striver's Array Series/MinimumJumps.java
@@ -2,15 +2,8 @@ public class MinimumJumps {
     static int minJumps(int[] arr, int n) {
         int jumps = 0;
         int steps = 0, maxReach = arr[0];
-        int max = 0;
         while(steps <= maxReach){
-            if(arr[maxReach + steps] + (maxReach + steps) > max) max = arr[maxReach + steps];
-            if(steps == maxReach){
-                maxReach = max;
-                jumps++;
-                steps = 0;
-            }
-            steps++;
+            
         }
         return jumps;
     }
